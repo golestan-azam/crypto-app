81b7010b573aac7cbd49a9c0b0763cbb3b5263eb
azam golestan <goli.goli499a.com>
2024-03-09 21:10:22 +0330

    Created Services Folder for manage APIs

--- src/components/templates/HomePage.jsx
+++ src/components/templates/HomePage.jsx
@@ -1,15 +1,22 @@
 import { useEffect, useState } from "react";
 import TableCoin from "../modules/TableCoin";
+import { getCoinList } from "../../services/cryptoApi";
 
 function HomePage(props) {
   const [coins, setCoins] = useState([]);
 
   useEffect(() => {
-    fetch(
-      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=false&locale=en&x_cg_demo_api_key=CG-mMrEJjRAt9VoNvZiLrieUC2n"
-    )
-      .then((res) => res.json())
-      .then((json) => setCoins(json));
+    // fetch(getCoinList())
+    //   .then((res) => res.json())
+    //   .then((json) => setCoins(json));
+
+    const getData = async () => {
+      const res = await fetch(getCoinList());
+      const json = await res.json();
+      setCoins(json);
+    };
+
+    getData();
   }, []);
   return (
     <div>
