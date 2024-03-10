e8443c31e8fc714e3348643ce29cda0e3c4d08d3
azam golestan <goli.goli499a.com>
2024-03-10 11:20:41 +0330

    Pagination Of Coin Data

--- src/components/templates/HomePage.jsx
+++ src/components/templates/HomePage.jsx
@@ -6,24 +6,28 @@ import Pagination from "../modules/Pagination";
 function HomePage(props) {
   const [coins, setCoins] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [page, setPage] = useState(1);
 
   useEffect(() => {
     // fetch(getCoinList())
     //   .then((res) => res.json())
     //   .then((json) => setCoins(json));
 
+    setIsLoading(true);
+
     const getData = async () => {
-      const res = await fetch(getCoinList());
+      const res = await fetch(getCoinList(page));
       const json = await res.json();
       setCoins(json);
       setIsLoading(false);
     };
 
     getData();
-  }, []);
+  }, [page]);
+
   return (
     <div>
-      <Pagination />
+      <Pagination page={page} setPage={setPage} />
       <TableCoin coins={coins} isLoading={isLoading} />
     </div>
   );
