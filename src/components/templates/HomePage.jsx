c7c2230d39c361edabaffc9b21a08ebddad4f075
azam golestan <goli.goli499a.com>
2024-03-13 17:09:45 +0330

    Changed Currency about Data

--- src/components/templates/HomePage.jsx
+++ src/components/templates/HomePage.jsx
@@ -2,11 +2,13 @@ import { useEffect, useState } from "react";
 import TableCoin from "../modules/TableCoin";
 import { getCoinList } from "../../services/cryptoApi";
 import Pagination from "../modules/Pagination";
+import Search from "../modules/Search";
 
 function HomePage(props) {
   const [coins, setCoins] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [page, setPage] = useState(1);
+  const [currecy, setCurrency] = useState("usd");
 
   useEffect(() => {
     // fetch(getCoinList())
@@ -16,17 +18,18 @@ function HomePage(props) {
     setIsLoading(true);
 
     const getData = async () => {
-      const res = await fetch(getCoinList(page));
+      const res = await fetch(getCoinList(page, currecy));
       const json = await res.json();
       setCoins(json);
       setIsLoading(false);
     };
 
     getData();
-  }, [page]);
+  }, [page,currecy]);
 
   return (
     <div>
+      <Search currecy={currecy} setCurrency={setCurrency} />
       <TableCoin coins={coins} isLoading={isLoading} />
       <Pagination page={page} setPage={setPage} />
     </div>
