247b560cb25d7ca6e5ada5dab41afc50c04e1ae1
azam golestan <goli.goli499a.com>
2024-03-15 22:52:56 +0330

    Show the searched Data

--- src/components/modules/Search.jsx
+++ src/components/modules/Search.jsx
@@ -8,6 +8,9 @@ function Search({ currency, setCurrency }) {
 
   useEffect(() => {
     const controller = new AbortController();
+
+    setCoins([]);
+    
     if (!text) return;
 
     const search = async () => {
@@ -47,6 +50,16 @@ function Search({ currency, setCurrency }) {
         <option value="eur">EUR</option>
         <option value="jpy">JPY</option>
       </select>
+      <div>
+        <ul>
+          {coins.map((coin) => (
+            <li key={coin.id}>
+              <img src={coin.thumb} alt={coin.name} />
+              <p>{coin.name}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
