e964e7e5449b9081ccc94ee8c939d5006b4a045d
azam golestan <goli.goli499a.com>
2024-03-15 23:09:24 +0330

    Show Loading During Search

--- src/components/modules/Search.jsx
+++ src/components/modules/Search.jsx
@@ -1,17 +1,22 @@
-import { useEffect } from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { RotatingLines } from "react-loader-spinner";
+
 import { searchCoin } from "../../services/cryptoApi";
 
 function Search({ currency, setCurrency }) {
   const [text, setText] = useState("");
   const [coins, setCoins] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const controller = new AbortController();
 
     setCoins([]);
-    
-    if (!text) return;
+
+    if (!text) {
+      setIsLoading(false);
+      return;
+    }
 
     const search = async () => {
       try {
@@ -22,6 +27,7 @@ function Search({ currency, setCurrency }) {
         console.log(json);
         if (json.coins) {
           setCoins(json.coins);
+          setIsLoading(false);
         } else {
           alert(json.status.error_message);
         }
@@ -32,6 +38,7 @@ function Search({ currency, setCurrency }) {
       }
     };
 
+    setIsLoading(true);
     search();
 
     return () => controller.abort();
@@ -51,6 +58,14 @@ function Search({ currency, setCurrency }) {
         <option value="jpy">JPY</option>
       </select>
       <div>
+        {isLoading && (
+          <RotatingLines
+            width="50px"
+            height="50px"
+            strokeWidth="2"
+            strokeColor="#3874ff"
+          />
+        )}
         <ul>
           {coins.map((coin) => (
             <li key={coin.id}>
