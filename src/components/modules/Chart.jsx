10b36a86b8c46789f62642fc2a27e843f60f9aa7
azam golestan <goli.goli499a.com>
2024-03-17 09:35:22 +0330

    Changing the Data Format to Draw the Graph

--- src/components/modules/Chart.jsx
+++ src/components/modules/Chart.jsx
@@ -1,10 +1,16 @@
+import { useState } from "react";
+import { convertData } from "../../helpers/convertData";
 import styles from "./Chart.module.css";
 
 function Chart({ chart, setChart }) {
-  console.log(chart);
+  const [type, setType] = useState("prices");
+  console.log(convertData(chart, type));
+
   return (
     <div className={styles.container}>
-      <span className={styles.cross} onClick={() => setChart(null)}>X</span>
+      <span className={styles.cross} onClick={() => setChart(null)}>
+        X
+      </span>
       <div className={styles.chart}></div>
     </div>
   );
